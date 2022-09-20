c9e8bf6f3e1ccb11016b2af08b5ed91c40c8f903
Adebanjo Olumide <106744505+LumzyGH@users.noreply.github.com>
2022-09-20 20:15:11 +0100

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def fizzbuzz():
+  def fizzbuzz():
     for i in range(1, 101):
         if (i % 5 == 0) and (i % 3 == 0):
             print('FizzBuzz ', end='')
@@ -9,4 +9,3 @@ def fizzbuzz():
             print('Buzz ', end='')
         else:
             print('{} '.format(i), end='')
-
